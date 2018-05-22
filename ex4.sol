ade73ea9aa65ecc3ede9cf5bf2a2343c04f5a30c
Jacques Chevrier <34470570+jacchevr@users.noreply.github.com>
2018-05-22 10:24:31 -0600

    Update ex4.sol

--- ex4.sol
+++ ex4.sol
@@ -1,1 +1,14 @@
-# example4-solidity
+pragma solidity ^0.4.0;
+
+import {A as B} from './importing_from.sol';
+
+import "https://github.com/OpenZeppelin/zeppelin-solidity/contracts/token/ERC721/ERC721Token.sol";
+
+contract MyContract is ERC721Token {
+    
+    function MyContract() public {
+        B contractA = new B();
+        contractA.myVarA();
+    }
+    
+}
